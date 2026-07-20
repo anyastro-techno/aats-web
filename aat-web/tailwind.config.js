a1bf9be36d4f2a548f2f5468631957a5d5570e67
Arun Ammisetty <gchk@duck.com>
2026-07-20 18:53:42 +0000

    build: register dyslexia-friendly typography profile within visual processing engine for accessibility compliance

--- aat-web/tailwind.config.js
+++ aat-web/tailwind.config.js
@@ -8,6 +8,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
+        dyslexic: ['OpenDyslexic', 'Comic Sans MS', 'sans-serif'],
       },
     },
   },
